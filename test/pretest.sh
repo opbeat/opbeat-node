864d0237098242bd5b1c6388bb98f563066ae71e
Thomas Watson <w@tson.dk>
2017-09-11 14:40:15 +0200

    test(*): don't kill all databases after testing
    Be smarter about stopping database after local testing.
    
    For MongoDB we can use a pid file and for PostgreSQL we instead use the
    pg_ctl command to control the database as a daemon.

--- test/pretest.sh
+++ test/pretest.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 if [ "$TRAVIS" != "true" ]; then
-  postgres -D /usr/local/var/postgres >/tmp/postgres.log 2>&1 &
-  mongod --fork --config /usr/local/etc/mongod.conf >/tmp/mongod.log 2>&1
+  pg_ctl -D /usr/local/var/postgres start
+  mongod --fork --config /usr/local/etc/mongod.conf --pidfilepath /tmp/mongod.pid >/tmp/mongod.log 2>&1
   redis-server /usr/local/etc/redis.conf --daemonize yes
   mysql.server start
 fi
